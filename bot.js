16953507410369dfd4b77af9b409af154ba9f90e
ljoxloyd <lyudskoy@gmail.com>
2021-03-16 16:34:33 +0300

    texting express

--- bot.js
+++ bot.js
@@ -1,26 +1,36 @@
 import TelegramBot from 'node-telegram-bot-api';
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, _ => console.log('listening on port' + port))
+app.get("/", (req, res) => {
+  res.send({ ok: true });
+})
 
 const token = process.env.BOT_TOKEN
 
 const options = { polling: true }
 
-const bot = new TelegramBot(token, options);
+// const bot = new TelegramBot(token, options);
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+// // Matches "/echo [whatever]"
+// bot.onText(/\/echo (.+)/, (msg, match) => {
 
-  const chatId = msg.chat.id;
-  const resp = match[1];
+//   const chatId = msg.chat.id;
+//   const resp = match[1];
 
-  bot.sendMessage(chatId, resp);
-});
+//   bot.sendMessage(chatId, resp);
+// });
 
-bot.onText(/\/keys/, msg => {
-  bot.sendMessage(msg.chat.id, "here's your keyboar", {
-    "reply_markup": {
-      "inline_keyboard": [
-        [{ text: "Homie", url: "https://thispersondoesnotexist.com/" }]
-      ]
-    }
-  })
-})
+// bot.onText(/\/keys/, msg => {
+//   bot.sendMessage(msg.chat.id, "here's your keyboar", {
+//     "reply_markup": {
+//       "inline_keyboard": [
+//         [{ text: "Homie", url: "https://thispersondoesnotexist.com/" }]
+//       ]
+//     }
+//   })
+// })
