645501ed541d3d59582f795c086c77d4857a8a95
ljoxloyd <lyudskoy@gmail.com>
2021-03-16 16:26:38 +0300

    init commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.BOT_TOKEN
+
+const options = { polling: true }
+
+const bot = new TelegramBot(token, options);
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/keys/, msg => {
+  bot.sendMessage(msg.chat.id, "here's your keyboar", {
+    "reply_markup": {
+      "inline_keyboard": [
+        [{ text: "Homie", url: "https://thispersondoesnotexist.com/" }]
+      ]
+    }
+  })
+})
