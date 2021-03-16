276b7a42324a6fcbeeb209786d04a9de70f3d35f
ljoxloyd <lyudskoy@gmail.com>
2021-03-16 16:41:17 +0300

    webhooks ready

--- bot.js
+++ bot.js
@@ -1,36 +1,38 @@
 import TelegramBot from 'node-telegram-bot-api';
 import express from 'express';
 
-const app = express();
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, _ => console.log('listening on port' + port))
-app.get("/", (req, res) => {
-  res.send({ ok: true });
-})
+// const app = express();
+// const port = process.env.PORT || 5000;
+// app.listen(port, _ => console.log('listening on port' + port))
+// app.get("/", (req, res) => {
+//   res.send({ ok: true });
+// })
 
 const token = process.env.BOT_TOKEN
+const url = process.env.APP_URL 
+const options = { webhook: {
+  port: process.env.PORT
+}}
 
-const options = { polling: true }
+const bot = new TelegramBot(token, options);
 
-// const bot = new TelegramBot(token, options);
+bot.setWebHook(`${url}/bot${token}`);
 
-// // Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
 
-//   const chatId = msg.chat.id;
-//   const resp = match[1];
+  const chatId = msg.chat.id;
+  const resp = match[1];
 
-//   bot.sendMessage(chatId, resp);
-// });
+  bot.sendMessage(chatId, resp);
+});
 
-// bot.onText(/\/keys/, msg => {
-//   bot.sendMessage(msg.chat.id, "here's your keyboar", {
-//     "reply_markup": {
-//       "inline_keyboard": [
-//         [{ text: "Homie", url: "https://thispersondoesnotexist.com/" }]
-//       ]
-//     }
-//   })
-// })
+bot.onText(/\/keys/, msg => {
+  bot.sendMessage(msg.chat.id, "here's your keyboar", {
+    "reply_markup": {
+      "inline_keyboard": [
+        [{ text: "Homie", url: "https://thispersondoesnotexist.com/" }]
+      ]
+    }
+  })
+})
